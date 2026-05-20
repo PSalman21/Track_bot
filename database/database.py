8d7e8dd7abb0a63470fc72451ce3c951e9b15143
PSalman21 <pogorvsalman21@gmail.com>
2026-05-20 10:15:42 +0300

    Обновление database.py
    Создал функцию для добавления настроения дня

--- database/database.py
+++ database/database.py
@@ -55,4 +55,39 @@ class Database:
         self.conn.commit()
 
         return result
-    
+        
+    def add_entry(
+        self,
+        user_id,
+        mood,
+        work,
+        sleep,
+        comment
+    ):
+
+        query = """
+        INSERT INTO mood_entries
+        (
+            user_id,
+            mood,
+            hours_work,
+            hours_sleep,
+            comment
+        )
+
+        VALUES (?, ?, ?, ?, ?)
+
+        ON CONFLICT(user_id, entry_date)
+        DO UPDATE SET
+            mood = excluded.mood,
+            hours_work = excluded.hours_work,
+            hours_sleep = excluded.hours_sleep,
+            comment = excluded.comment
+        """
+
+        self.execute(
+            query,
+            (user_id, mood, work, sleep, comment)
+        )
+
+
